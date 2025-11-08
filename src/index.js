c7d2704fc5ac6f53e7efd3d34cab3fc3b8f9e1f3
ivan <ivanlowlh@gmail.com>
2025-11-08 10:46:08 +0800

    Update buttons to link to pages

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 import "./styles.css";
 import { homePage } from "./homepage";
+import { menuPage } from "./menu";
+
 
 homePage();
+
+function clearContent () {
+    const contentDiv = document.querySelector("#content");
+    contentDiv.textContent = "";
+}
+
+const homeButton = document.getElementById("home-btn");
+homeButton.addEventListener("click", () => {
+    clearContent();
+    homePage();
+})
+
+const menuButton = document.getElementById("menu-btn");
+menuButton.addEventListener("click", () => {
+    clearContent();
+    menuPage();
+})
